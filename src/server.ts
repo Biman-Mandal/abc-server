3bbfdd9ceab1547a6c4112c3fcfc8eed25a30344
== <bimanm193@gmail.com>
2025-09-25 15:55:32 +0530

    fixes on accept ride

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import { app } from "./app";
 import http from "http";
 import { initSocket } from "./app/socket/socket";
 import mongoDb from "./app/config/db";
+import User from "./app/models/user.model";
+import {Driver} from "./app/models/driver.model";
 
 const PORT = process.env.PORT || 8080;
 
@@ -12,9 +14,40 @@ const server = http.createServer(app);
 
 const io = initSocket(server);
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log(`New client connected: ${socket.id}`);
+  const userId = socket.handshake.query.userId as string;
+  const isDriver = socket.handshake.query.isDriver as string;
+  if (!userId) {
+    console.log("No userId provided. Disconnecting socket:", socket.id);
+    socket.disconnect();
+    return;
+  }
 
+  if(isDriver){
+    // Check if user exists in DB
+    const driverExits = await Driver.exists({ _id: userId });
+    if (!driverExits) {
+      console.log(
+        `Driver ID ${userId} does not exist. Disconnecting socket:`,
+        socket.id
+      );
+      socket.disconnect();
+      return;
+    }
+  }else{
+    // Check if user exists in DB
+    const userExists = await User.exists({ _id: userId });
+    if (!userExists) {
+      console.log(
+        `User ID ${userId} does not exist. Disconnecting socket:`,
+        socket.id
+      );
+      socket.disconnect();
+      return;
+    }
+  }
+  socket.join(userId);
   socket.on("updateLocation", (data) => {
     socket.broadcast.emit("driverLocationUpdated", data);
   });
